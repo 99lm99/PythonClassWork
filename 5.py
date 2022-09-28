279555b667a718df571bab9be697e1e76936a4b2
«Marya» <lezina96@inbox.ru>
2022-09-28 14:41:26 +0300

    add all

---
+++ 5.py
@@ -0,0 +1,9 @@
+#Напишите программу, которая принимает на вход число и проверяет, кратно ли оно 5 и 10 или 15, но не 30.
+
+number = int(input(' add number: '))
+
+print(number % 5 == 0 and number % 10 == 0 or number % 15 == 0 and number % 30 != 0)
+
+
+a = (float((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1))
+print(round(pow(a, 1/2)),2)
